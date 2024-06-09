b7cb7ea219a7af629fede073d327a5a5f98934c1
Asachuna <84565188+yugoka@users.noreply.github.com>
2024-06-09 21:53:12 +0900

    messagesをEdge Functionに変更

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -1,6 +1,7 @@
 import { getMessagesByThreadID } from "@/services/messages";
 import { Message } from "@/types/Message";
 import { NextResponse } from "next/server";
+export const runtime = "edge";
 
 export async function GET(req: Request) {
   try {
