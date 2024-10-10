df1ccf42165ffe33287577515fc9c4d9529fd47c
Asachuna <84565188+yugoka@users.noreply.github.com>
2024-10-10 17:02:49 +0900

    add toolcall types/create api

---
+++ src/services/tool_calls.ts
@@ -0,0 +1,48 @@
+import { Message } from "@/types/Message";
+import { ToolCall } from "@/types/ToolCall";
+import { stringfyMessagesForLM } from "@/utils/message";
+import { createClient } from "@/utils/supabase/server";
+import { getEmbedding } from "./embeddings";
+
+// ==============
+// ToolCall作成
+// ==============
+export interface CreateToolCallInput {
+  id?: string;
+  tool_id: string;
+  tool_call_id: string;
+  execution_time: number;
+  contextMessages: Message[];
+}
+export const createToolCall = async ({
+  id,
+  tool_call_id,
+  tool_id,
+  execution_time,
+  contextMessages,
+}: CreateToolCallInput): Promise<ToolCall> => {
+  const contextText = stringfyMessagesForLM(contextMessages);
+  const contextEmbedding = await getEmbedding(contextText);
+
+  const supabase = createClient();
+
+  const { data, error } = await supabase
+    .from("tool_calls")
+    .insert([
+      {
+        id,
+        tool_call_id,
+        tool_id,
+        execution_time,
+        context_embedding: contextEmbedding,
+      },
+    ])
+    .select("*")
+    .single();
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
+};
