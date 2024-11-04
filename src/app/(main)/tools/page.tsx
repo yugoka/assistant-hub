f95f8395e61ff10388cbb2cd11d9d1ca97f0b845
Asachuna <84565188+yugoka@users.noreply.github.com>
2024-11-05 02:53:43 +0900

    ツールページの初期案内追加

--- src/app/(main)/tools/page.tsx
+++ src/app/(main)/tools/page.tsx
@@ -1,3 +1,21 @@
+import { ToolIcon } from "@/components/common/icons/ToolIcon";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+import Link from "next/link";
+
 export default async function ToolsPage() {
-  return <>This is Tools Page</>;
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <span className="mb-4 text-lg font-medium text-gray-700 dark:text-gray-400">
+        <ToolIcon className="inline-block w-5 h-5 mb-1 mr-1" /> Select a tool or
+        create a new one
+      </span>
+      <Link href="/tools/new">
+        <Button className="flex items-center">
+          <PlusIcon className="mr-2 h-4 w-4" />
+          Create new tool
+        </Button>
+      </Link>
+    </div>
+  );
 }
