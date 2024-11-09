7791fe359388239ffa32f526ff0f3af8b6cb8015
Asachuna <84565188+yugoka@users.noreply.github.com>
2024-11-09 14:53:42 +0900

    contexts/にスタイリングが適用されていなかった問題を修正

--- src/contexts/NavigaitonContext.tsx
+++ src/contexts/NavigaitonContext.tsx
@@ -48,7 +48,7 @@ export function SharedNavMenu({
 
   return (
     <>
-      <div className="hidden w-64 shrink-0 border-r bg-gray-100 dark:border-gray-800 dark:bg-gray-900 md:block">
+      <div className="hidden md:block shrink-0 border-r bg-gray-100 dark:border-gray-800 dark:bg-gray-900">
         <div className="flex h-full flex-col justify-between pt-6">
           {NavMenu}
         </div>
